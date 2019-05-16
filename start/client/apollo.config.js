565dadf0e060b4c4c17caf16275713644c3784e5
Paul Joey Clark <joeytwiddle@gmail.com>
2019-05-16 16:41:34 +0800

    Set up client skeleton

---
+++ start/client/apollo.config.js
@@ -0,0 +1,7 @@
+// For VSCode and cmdline
+module.exports = {
+  client: {
+    name: 'Space Explorer [web]',
+    service: 'space-explorer',
+  },
+};
---
+++ start/client/src/index.js
@@ -0,0 +1,27 @@
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ReactDOM from 'react-dom';
+import Pages from './pages';
+
+const cache = new InMemoryCache();
+
+const link = new HttpLink({
+    uri: 'http://localhost:4000/'
+})
+
+const client = new ApolloClient({
+    cache,
+    link
+})
+
+//client.query({ query: gql` query GetLaunch { launch(id: 56) { id mission { name } } } ` }).then(result => console.log('result:', result));
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Pages />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
