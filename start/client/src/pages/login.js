7cc4a36fac8679d7d2787c682d0c2f3e85011c27
Paul Joey Clark <joeytwiddle@gmail.com>
2019-05-17 17:40:08 +0800

    Add login form, and update local state

---
+++ start/client/src/pages/login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Mutation, ApolloConsumer } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { LoginForm, Loading } from '../components';
+
+const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
+
+/*
+// Simple implementation
+export default function Login() {
+  return (
+    <Mutation mutation={LOGIN_USER}>
+      {(login, { data }) => <LoginForm login={login} />}
+    </Mutation>
+  );
+}
+*/
+
+// Getting the client so we can write to the local cache
+export default function Login() {
+  return (
+    <ApolloConsumer>
+      {client => (
+        <Mutation
+          mutation={LOGIN_USER}
+          onCompleted={({ login }) => {
+            localStorage.setItem('token', login);
+            client.writeData({ data: { isLoggedIn: true } });
+          }}
+        >
+          {(login, { loading, error }) => {
+            // this loading state will probably never show, but it's helpful to
+            // have for testing
+            if (loading) return <Loading />;
+            if (error) return <p>An error occurred</p>;
+
+            return <LoginForm login={login} />;
+          }}
+        </Mutation>
+      )}
+    </ApolloConsumer>
+  );
+}
