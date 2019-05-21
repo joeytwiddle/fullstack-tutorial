bc636b4362be6ef8f3ed7c4718848da6a7464630
Paul Joey Clark <joeytwiddle@gmail.com>
2019-05-21 18:42:40 +0800

    Re-arrange files to look more similar to those in the final/ folder

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,36 +1,42 @@
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
+
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 const { createStore } = require('./utils');
+
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
-const resolvers = require('./resolvers');
-const isEmail = require('isemail');
 
 const store = createStore();
 
+const dataSources = () => ({
+  launchAPI: new LaunchAPI(),
+  userAPI: new UserAPI({ store }),
+});
+
+const context = async ({ req }) => {
+  // simple auth check on every request
+  // Not for use in real world applications.  It just accepts an email address and trusts it!
+  const auth = (req.headers && req.headers.authorization) || '';
+  const email = Buffer.from(auth, 'base64').toString('ascii');
+
+  // if the email isn't formatted validly, return null for user
+  if (!isEmail.validate(email)) return { user: null };
+  // find a user by their email
+  const users = await store.users.findOrCreate({ where: { email } });
+  const user = users && users[0] ? users[0] : null;
+
+  return { user: { ...user.dataValues } };
+};
+
 const server = new ApolloServer({
-    context: async ({ req }) => {
-        // simple auth check on every request
-        // Not for use in real world applications.  It just accepts an email address and trusts it!
-        const auth = (req.headers && req.headers.authorization) || '';
-        const email = Buffer.from(auth, 'base64').toString('ascii');
-
-        // if the email isn't formatted validly, return null for user
-        if (!isEmail.validate(email)) return { user: null };
-        // find a user by their email
-        const users = await store.users.findOrCreate({ where: { email } });
-        const user = users && users[0] ? users[0] : null;
-
-        return { user: { ...user.dataValues } };
-    },
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        launchAPI: new LaunchAPI(),
-        userAPI: new UserAPI({ store })
-    })
+  context,
+  typeDefs,
+  resolvers,
+  dataSources,
 });
 
 server.listen().then(({ url }) => {
-    console.log("Listening at:", url);
+  console.log("Listening at:", url);
 });
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
   type Query {
     # Before pagination
     #launches: [Launch]!
- 
+
     # With pagination
     launches( # replace the current launches query with this one.
       """
@@ -16,9 +16,25 @@ const typeDefs = gql`
       """
       after: String
     ): LaunchConnection!
-      
+
     launch(id: ID!): Launch
     me: User
+  }
+
+  type Mutation {
+    # if false, booking trips failed -- check errors
+    bookTrips(launchIds: [ID]!): TripUpdateResponse!
+
+    # if false, cancellation failed -- check errors
+    cancelTrip(launchId: ID!): TripUpdateResponse!
+
+    login(email: String): String # login token
+  }
+
+  type TripUpdateResponse {
+    success: Boolean!
+    message: String
+    launches: [Launch]
   }
 
   """
@@ -40,45 +56,28 @@ const typeDefs = gql`
     rocket: Rocket
     isBooked: Boolean!
   }
-  
+
   type Rocket {
     id: ID!
     name: String
     type: String
   }
-  
+
   type User {
     id: ID!
     email: String!
     trips: [Launch]!
   }
-  
+
   type Mission {
     name: String
     missionPatch(size: PatchSize): String
   }
-  
+
   enum PatchSize {
     SMALL
     LARGE
   }
-  
-  type Mutation {
-    # if false, booking trips failed -- check errors
-    bookTrips(launchIds: [ID]!): TripUpdateResponse!
-  
-    # if false, cancellation failed -- check errors
-    cancelTrip(launchId: ID!): TripUpdateResponse!
-  
-    login(email: String): String # login token
-  }
-  
-  type TripUpdateResponse {
-    success: Boolean!
-    message: String
-    launches: [Launch]
-  }
-
 `;
 
 module.exports = typeDefs;
